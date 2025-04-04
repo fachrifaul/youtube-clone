2543860895522a515f0d254e07f91eebec13ecd8
fachrifaul <fachri.febrian@bukalapak.com>
2025-04-04 23:37:39 +0700

    add generate upload url

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,5 +1,6 @@
-/* eslint-disable object-curly-spacing, indent, semi, eol-last */
+/* eslint-disable */
 
+import { Storage } from "@google-cloud/storage";
 import { initializeApp } from "firebase-admin/app";
 import { Firestore } from "firebase-admin/firestore";
 import * as functions from "firebase-functions";
@@ -7,9 +8,12 @@ import * as logger from "firebase-functions/logger";
 
 initializeApp();
 
+const region = functions.region("asia-southeast1");
 const firestore = new Firestore();
+const storage = new Storage();
+const rawVideoBucketName = "nc-yt-raw-video";
 
-export const createUser = functions.region("asia-southeast1").auth.user().onCreate((user) => {
+export const createUser = region.auth.user().onCreate((user) => {
     const userInfo = {
         uid: user.uid,
         email: user.email,
@@ -22,4 +26,33 @@ export const createUser = functions.region("asia-southeast1").auth.user().onCrea
     logger.info(`User created: ${JSON.stringify(userInfo)}`);
 
     return;
+});
+
+export const generateUploadUrl = region.https.onCall(async (request) => {
+
+    if (!request.auth) {
+        throw new functions.https.HttpsError(
+            "unauthenticated",
+            "The function must be called while authenticated."
+        )
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    // generate unique filemane
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    // get a v4 signed url for uploading file
+    const [url] = await bucket
+        .file(fileName)
+        .getSignedUrl({
+            version: "v4",
+            action: "write",
+            expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+        });
+
+    return { url, fileName };
 });
